6b46b3ad70cfca56dd2404c37e848e90aaf0de22
Elijah Kalosa <124288198+kalosa0770@users.noreply.github.com>
2026-08-09 07:03:26 +0200

    worked on sections

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,17 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen bg-background py-12 md:py-24">
       <JuicerFeedProvider feedId="atmosphere-daily">
-        {/* Captures #news */}
-        <JuicerHashtagSection tag="#news" title="Daily News" />
+
+        {/* Captures all posts that do NOT have any hashtags */}
+        <UntaggedPostsSection title="Daily News" />
+
+        {/* Captures #spotlight */}
+        <JuicerHashtagSection tag="#news" title="Spotlight News" />
 
         {/* Captures #blog */}
         <JuicerHashtagSection tag="#blog" title="Blogs & Articles" />
 
-        {/* Captures all posts that do NOT have any hashtags */}
-        <UntaggedPostsSection title="General Updates" />
+        
       </JuicerFeedProvider>
     </main>
   );
